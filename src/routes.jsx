a1a0c1178b08148794e3370e379683d89ca40880
AVega89-0407 <andreavega89.av@gmail.com>
2026-05-12 10:55:59 +0200

    built routes

---
+++ src/routes.jsx
@@ -0,0 +1,11 @@
+import { createElement } from "react";
+
+
+const pages = import.meta.glob('./pages/*.jsx', { eager: true });
+
+const routes = Object.values(pages)
+  .map(x => x.default)
+  .map(x => ({ ...x.route, element: createElement(x) }))
+  .sort((a, b) => a.index - b.index);
+
+export default routes;
