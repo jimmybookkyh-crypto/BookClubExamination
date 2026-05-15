cc17c47ed22e1fe3d13b468191bd581564055ab0
sarakemppi <kemppisara@gmail.com>
2026-05-15 10:30:07 +0200

    built heade rcomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import { Outlet } from 'react-router'
+import { useState } from "react";
+import { Outlet } from "react-router";
+import Header from "./UI/header";
 
 export default function App() {
   return (
-  <>
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
-
