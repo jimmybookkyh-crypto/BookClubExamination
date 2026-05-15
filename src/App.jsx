ad335f7fa9a7b081373dcded1ebdb89f150b64ba
jimmybookkyh-crypto <jimmybookkyh@gmail.com>
2026-05-15 14:45:18 +0200

    built login/register framework

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Outlet } from "react-router";
 import Header from "./UI/header";
 
 export default function App() {
+  const [user, setUser] = useState(localStorage.user ?
+    JSON.parse(localStorage.user) : null);  
+  
   return (
     <>
-      <Header />
+      <Header user={user} setUser={setUser} />
       <main>
-        <Outlet />
+        <Outlet context={{ user, setUser }} />
       </main>
     </>
   );
