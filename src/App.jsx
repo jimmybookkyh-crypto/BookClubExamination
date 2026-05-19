d16ee0cbc1c2fde7cfa0f53b6341dd2afc7a065b
sarakemppi <kemppisara@gmail.com>
2026-05-19 11:58:23 +0200

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { Outlet } from "react-router";
 import Header from "./UI/header";
 
 export default function App() {
-  const [user, setUser] = useState(localStorage.user ?
-    JSON.parse(localStorage.user) : null);  
-  
+  const [user, setUser] = useState(
+    localStorage.user ? JSON.parse(localStorage.user) : null,
+  );
+
   return (
     <>
       <Header user={user} setUser={setUser} />
---
+++ src/ui/NavBar.jsx
@@ -0,0 +1,39 @@
+import routes from "../routes";
+import { NavLink } from "react-router";
+
+export default function NavBar({ user, setUser }) {
+  function logout() {
+    delete localStorage.user;
+    setUser(null);
+  }
+
+  return (
+    <nav>
+      {routes
+        .filter((x) => x.label)
+        .map(({ path, label }) => (
+          <NavLink to={path} key={path}>
+            {label}
+          </NavLink>
+        ))}
+      <div className="auth">
+        {user ? (
+          <>
+            Inloggad som{" "}
+            <NavLink to="/profile">
+              <b>{user.user.username}</b>
+            </NavLink>
+            &nbsp;&nbsp;
+            <button onClick={logout}>Logga ut</button>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Logga in</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/register">Skapa konto</NavLink>{" "}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
