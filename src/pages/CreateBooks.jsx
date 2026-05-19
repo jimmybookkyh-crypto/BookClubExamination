20a73d4010815a0641429b90a8604ba5c9133c08
jimmybookkyh-crypto <jimmybookkyh@gmail.com>
2026-05-19 16:41:36 +0200

    built CreateBooks page(let users  Post new book title), built checkBookTitle util(checks if title already in db before Post)

---
+++ src/pages/CreateBooks.jsx
@@ -0,0 +1,203 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import useFetch from "../utils/useFetch";
+import checkBookTitle from "../utils/checkBookTitle";
+
+CreateBook.route = {
+  path: '/create-book'
+}
+
+const formInitialState = {
+  title: "",
+  author: "0",
+  newAuthorFirstName: "",
+  newAuthorLastName: "",
+  genre: "0",
+  year: "",
+  description: ""
+};
+
+export default function CreateBook() {
+  const [formData, setFormData] = useState(formInitialState);
+  const [formSent, setFormSent] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const [bookAuthors, bookGenres, loading] = useFetch(
+    "/api/authors?pagination[pageSize]=1000&sort=firstName,lastName",
+    "/api/genres?pagination[pageSize]=1000"
+  );  
+
+  const [showAuthorInput, setShowAuthorInput] = useState(false);
+  
+  if (loading) { return <p>Laddar...</p>; }
+  
+  function updateFormData(event) {
+    const { name: key, value } = event.target;
+    setFormData({ ...formData, [key]: value });
+  }
+
+async function sendForm(event) {
+
+  event.preventDefault();
+  setError('');
+
+  // måste välja author
+if (!showAuthorInput && formData.author === "0") {
+  setError("Du måste välja eller skapa en författare.");
+  return;
+}
+
+  // kontroll för ny author
+  if ( showAuthorInput &&
+    (
+      !formData.newAuthorFirstName.trim() ||
+      !formData.newAuthorLastName.trim()
+    )
+  ) {
+    setError("Fyll i både förnamn och efternamn.");
+    return;
+  }
+
+  // måste välja en genre
+    if (
+    formData.genre === "0" ||
+    formData.genre === ""
+  )
+  {
+    setError("Du måste välja en genre.");
+    return;
+  }
+  const titleExists = await checkBookTitle(formData.title);
+
+  if (titleExists) {
+    setError('En bok med den titeln finns redan.');
+    return;
+  }
+
+  let authorId = formData.author;
+
+  // skapa ny author
+  if (showAuthorInput) {
+
+    const authorResponse = await fetch('/api/authors', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        data: {
+          firstName: formData.newAuthorFirstName,
+          lastName: formData.newAuthorLastName
+        }
+      })
+
+    });
+    const authorData = await authorResponse.json();
+
+    authorId = authorData.data.documentId;
+  }
+
+  // skapa ny book
+const response = await fetch('/api/books', {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify({
+    data: {
+      title: formData.title,
+      year: formData.year,
+      description: formData.description,
+      genres: formData.genre === '0' ? null: formData.genre,
+      author: authorId === '0' ? null: authorId
+    }
+  })
+});
+
+const data = await response.json();
+
+console.log(data);
+
+  setFormSent(true);
+}
+
+function setAuthor(event) {
+
+  const isNew = event.target.value === "__new__";
+
+  setShowAuthorInput(isNew);
+
+  setFormData({
+    ...formData,
+    author: isNew ? "" : event.target.value
+  });
+}
+
+  if (formSent) {
+    return <>
+      <p> Boken {formData.title} har blivit tillagd.</p>
+      <button onClick={() => {
+        setFormSent(false);
+        setFormData({ ...formInitialState });
+      }}> Lägg till en till bok</button>
+      <button onClick={() => navigate('/')}>
+        Gå till startsida</button>
+    </>;
+    
+  } else {
+
+    return <>
+      <h2>Lägg till en ny bok</h2>
+      <form onSubmit={sendForm}>
+        <label>
+          Title:
+          <input required name="title" type="text" placeholder="titel" value={formData.title} onChange={updateFormData} />
+        </label>
+        <label>
+          Author:
+          <select name="author" value={formData.author} onChange={setAuthor}>
+          <option value="0">Välj författare</option>
+            {
+              bookAuthors.map(({ documentId, firstName, lastName }) => (
+                <option key={documentId} value={documentId}>
+                  {firstName} {lastName}
+                </option>
+              ))
+            }
+            <option value="__new__">Lägg till ny författare...</option>
+          </select>
+          { showAuthorInput && (
+              <>
+                <label>
+                  Förnamn:
+                  <input required name="newAuthorFirstName" type="text" value={formData.newAuthorFirstName} onChange={updateFormData} />                  
+                </label>
+                <label>
+                  Efternamn:
+                  <input required name="newAuthorLastName" type="text" value={formData.newAuthorLastName} onChange={updateFormData} />
+                </label>
+              </>)
+          }
+        </label>
+        <label>
+          Genre:
+          <select name="genre" value={formData.genre} onChange={updateFormData}>
+          <option value="0">Välj genre</option>
+            {bookGenres.map(({ documentId, name }) =>
+              <option key={documentId} value={documentId}>
+                {name}
+              </option>
+            )}
+          </select>
+        </label>
+        <label>
+          Utgivningsår:
+          <input required name="year" type="number" placeholder="Utgivningsår" value={formData.year} onChange={updateFormData} />
+        </label>
+        <label>
+          Beskrivning:
+          <input required name="description" type="text" placeholder="Beskrivning" value={formData.description} onChange={updateFormData} />
+        </label>
+        <button type="submit">Lägg till bok</button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
+    </>;
+    
+  }
+}
