2992d50005c7eec396c8748dbc41ee4e3b2e551c
MrsKoning <evamaria.koning@gmail.com>
2026-05-25 14:43:50 +0200

    Created Custom Controll and route for review ratings.

--- strapi/src/api/review/controllers/review.js
+++ strapi/src/api/review/controllers/review.js
@@ -1,11 +1,13 @@
-'use strict';
+"use strict";
+
+const review = require("../routes/review");
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::review.review", ({ strapi }) => ({
   async create(ctx) {
     const userId = ctx.state.user.id;
-    
+
     const response = await strapi.entityService.create("api::review.review", {
       data: {
         ...ctx.request.body.data,
@@ -14,5 +16,21 @@ module.exports = createCoreController("api::review.review", ({ strapi }) => ({
     });
 
     return { data: response };
+  },
+  async findByRating(ctx) {
+    const { stars } = ctx.params;
+    if (stars < 1 || stars > 5) {
+      return ctx.badRequest("Betyg mellan 1 till 5");
+    }
+    const reviews = await strapi.entityService.findMany("api::review.review", {
+      filters: {
+        rating: Number(stars),
+      },
+      populate: {
+        user: true,
+        book: true,
+      },
+    });
+    return { data: reviews };
   },
 }));
