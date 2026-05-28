f7d8e895786f22dfe565bbe2d9518dd0e53c53b3
teddylindstrom <teddy.lindstrom.work@gmail.com>
2026-05-28 11:21:19 +0200

    Test of the startpage done

---
+++ src/test/Start.test.jsx
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import Start from "../pages/Start";
+
+describe("Start component", () => {
+  it("render test", () => {
+    render(
+      <MemoryRouter>
+        <Start />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/Welcome!/i)).toBeInTheDocument();
+  });
+});
