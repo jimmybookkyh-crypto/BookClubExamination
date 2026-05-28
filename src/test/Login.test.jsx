e2c3463d816ceebc957e846c7013617acd577b85
sarakemppi <kemppisara@gmail.com>
2026-05-28 11:22:26 +0200

    Add login test with vitest

---
+++ src/test/Login.test.jsx
@@ -0,0 +1,28 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import Login from "../pages/Login";
+
+vi.mock("react-router", async () => {
+  const actual = await vi.importActual("react-router");
+
+  return {
+    ...actual,
+    useNavigate: () => vi.fn(),
+    useOutletContext: () => ({
+      setUser: vi.fn(),
+    }),
+  };
+});
+
+describe("Login", () => {
+  it("render text", () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText(/Lösenord/i)).toBeInTheDocument();
+  });
+});
