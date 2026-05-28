a9798fc5ddac9545691ff844250fdea0218a03af
sarakemppi <kemppisara@gmail.com>
2026-05-28 12:43:44 +0200

    Lagt till paginering för sökfunktion

--- strapi/src/api/book/controllers/book.js
+++ strapi/src/api/book/controllers/book.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 /**
@@ -19,7 +18,7 @@ module.exports = createCoreController("api::book.book", ({ strapi }) => ({
         },
       },
       populate: {
-        genre: true,        
+        genre: true,
         author: true,
         image: true,
       },
@@ -32,6 +31,9 @@ module.exports = createCoreController("api::book.book", ({ strapi }) => ({
    */
   async searchBooks(ctx) {
     const { query } = ctx.params;
+
+    const page = Number(ctx.query.pagination?.page || 1);
+    const pageSize = Number(ctx.query.pagination?.pageSize || 16);
 
     const books = await strapi.entityService.findMany("api::book.book", {
       filters: {
@@ -57,50 +59,90 @@ module.exports = createCoreController("api::book.book", ({ strapi }) => ({
           },
         ],
       },
+
       populate: {
-        genre: true,        
+        genre: true,
         author: true,
         image: true,
       },
+
+      start: (page - 1) * pageSize,
+      limit: pageSize,
     });
 
-    return { data: books };
+    const total = await strapi.entityService.count("api::book.book", {
+      filters: {
+        $or: [
+          {
+            title: {
+              $containsi: query,
+            },
+          },
+          {
+            author: {
+              firstName: {
+                $containsi: query,
+              },
+            },
+          },
+          {
+            author: {
+              lastName: {
+                $containsi: query,
+              },
+            },
+          },
+        ],
+      },
+    });
+
+    return {
+      data: books,
+      meta: {
+        pagination: {
+          page,
+          pageSize,
+          pageCount: Math.ceil(total / pageSize),
+          total,
+        },
+      },
+    };
   },
-/**
- * book unique year controller
- */
-    async findUniqueYear(ctx) {
-    const books = await strapi.documents('api::book.book').findMany({
-      fields: ['year']
+
+  /**
+   * book unique year controller
+   */
+  async findUniqueYear(ctx) {
+    const books = await strapi.documents("api::book.book").findMany({
+      fields: ["year"],
     });
 
-    const year = [...new Set(books
-      .map(book => book.year))] 
-      .filter(year => year) 
-      .sort();                   
+    const year = [...new Set(books.map((book) => book.year))]
+      .filter((year) => year)
+      .sort();
 
     // ctx.body = what you want return from your route
     ctx.body = { data: year };
   },
-    
+
   /**
    * book year controller
    */
-    
-    async findByYear(ctx) {
+
+  async findByYear(ctx) {
     const { year } = ctx.params;
     const books = await strapi.entityService.findMany("api::book.book", {
       filters: {
         year: {
-          $eq:Number(year),},
-         },    
+          $eq: Number(year),
+        },
+      },
       populate: {
-        genre: true,        
+        genre: true,
         author: true,
         image: true,
       },
     });
     return { data: books };
   },
-
 }));
